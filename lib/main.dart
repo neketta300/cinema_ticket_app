9acbb89dce7764636cf8e579018a353e8b4a3f4f
neketta300 <neketta300@gmail.com>
2025-03-11 21:08:34 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:cinema_ticket_app/Cinema%20App%20UI/pages/cinema_main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // Root widget of the app
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CinemaMainScreen(),
+    );
+  }
+}
+
+// for hive database
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Hive.initFlutter();
+//   await Hive.openBox("Box");
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: HiveDatabaseFlutter(),
+//     );
+//   }
+// }
